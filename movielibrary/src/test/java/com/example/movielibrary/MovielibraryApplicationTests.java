5e9b27f470940c0d53f820fa4902a0e39a55d851
Jasin93 <107134382+Jasin93@users.noreply.github.com>
2022-08-19 11:30:19 +0200

    Add files via upload

---
+++ movielibrary/src/test/java/com/example/movielibrary/MovielibraryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibraryApplicationTests {
+
+	@Test
+	void contextLoads()
+	{
+
+	}
+
+}
